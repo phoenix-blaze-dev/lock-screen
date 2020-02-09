5fc49caf3dae803f24beafede40738585f8402c1
phoenix-blaze-dev <phoenix.blaze.dev@outlook.com>
2020-02-09 21:14:28 +0100

    First commit

---
+++ app/src/main/java/com/example/lockscreen/LockButton.java
@@ -0,0 +1,62 @@
+package com.example.lockscreen;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
+import android.app.admin.DevicePolicyManager ;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class LockButton extends AppWidgetProvider {
+
+    DevicePolicyManager deviceManger;
+    ComponentName adminComponent;
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.lock_button);
+        // views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public void lockPhone (View view) {
+          if (!deviceManger.isAdminActive(adminComponent)) {
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, adminComponent);
+        } else {
+              deviceManger.lockNow();
+        }
+    }
+}
+
